efae523eaf6a273c7768d0ae2e8e683e4c3330a9
Collins Kiplimo <ckiplimo54@gmail.com>
2025-09-26 04:11:29 +0300

    first commit

---
+++ src/main/kotlin/com/collicode/cqrs_clean_architecture_kotlin/CqrsCleanArchitectureKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.collicode.cqrs_clean_architecture_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CqrsCleanArchitectureKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<CqrsCleanArchitectureKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/collicode/cqrs_clean_architecture_kotlin/CqrsCleanArchitectureKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.collicode.cqrs_clean_architecture_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CqrsCleanArchitectureKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
